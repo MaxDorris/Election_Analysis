ddfc191776754e9ee9ab0d3ddf1305fd7383d3fc
MaxDorris <58283761+MaxDorris@users.noreply.github.com>
2022-12-17 13:18:46 -0600

    Delete Practice_python.py

--- Practice_python.py
+++
@@ -1,9 +0,0 @@
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-
-for county in counties_dict:
-    print(f'{county} county has {counties_dict[county]:,} registered voters.')
-
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
-
-for dictionary in voting_data:
-    print (f'{dictionary["county"]} county has {dictionary["registered_voters"]:,} registered voters.')
